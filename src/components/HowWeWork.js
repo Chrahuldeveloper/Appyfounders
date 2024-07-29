f6496f4ff61f9808da38e765335fb5de8246e397
Rahul <chrahulofficial@gmail.com>
2024-07-29 14:05:15 +0530

    done

--- src/components/HowWeWork.js
+++ src/components/HowWeWork.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsEmojiSmile } from "react-icons/bs";
 
 export default function HowWeWork() {
   const data = [
@@ -8,7 +9,7 @@ export default function HowWeWork() {
     },
     {
       Tittle: "User Research",
-      Para: "we conduct user research to understand your target audience.",
+      Para: "We begin with an in-depth discovery call to understand your business, goals, and vision for your website.",
     },
     {
       Tittle: "Testing & Launch",
@@ -21,12 +22,29 @@ export default function HowWeWork() {
   ];
 
   return (
-    <>
+    <div className="items-start mx-auto md:flex max-w-7xl">
       <div className="px-6 text-white ">
         <h1 className="text-2xl font-bold md:text-5xl md:max-w-lg">
           How we Work
         </h1>
       </div>
-    </>
+      <div className="flex flex-col items-center justify-center gap-6 px-2 md:px-3 md:w-2/3">
+        {data.map((item, i) => {
+          return (
+            <React.Fragment key={i}>
+              <div className="bg-[#272727] text-white max-w-xl rounded-xl shadow-lg p-8">
+                <div className="space-y-3">
+                  <div className="bg-[#aaeec4] text-black  rounded-full w-14 h-14 items-center flex justify-center">
+                    <BsEmojiSmile size={45} />
+                  </div>
+                  <h1 className="text-lg font-bold">{item.Tittle}</h1>
+                  <p className="leading-7">{item.Para}</p>
+                </div>
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </div>
   );
 }
